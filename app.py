1a76b20be9c8ca5ee9c3addb67808afb2e0461e2
Patrick Andrade <andrade.patrickreis@gmail.com>
2023-09-14 02:14:09 -0300

    Automação em Python

---
+++ app.py
@@ -0,0 +1,31 @@
+import pyautogui
+from time import sleep
+
+pyautogui.click(1026,352, duration=2)
+pyautogui.write('andrade.patrickreis@gmail.com')
+
+pyautogui.click(1078,436, duration=2)
+pyautogui.click(1046,494, duration=2)
+pyautogui.write('momozi@2022')
+pyautogui.click(1079,564, duration=2)
+
+ 
+ 
+ # abrir arquivo de texto
+
+with open ('produtos.txt', 'r') as arquivo:
+    for linha in arquivo:
+        produto = linha.split(',')[0]
+        quantidade = linha.split(',')[1]
+        preco = linha.split(',')[2]
+        
+        pyautogui.click(1079,564, duration=2)
+        pyautogui.write(produto)
+
+        pyautogui.click(1099,564, duration=2)
+        pyautogui.write(quantidade)
+
+        pyautogui.click(1179,564, duration=2)
+        pyautogui.write(preco)
+
+        sleep(1)
